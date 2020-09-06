068d19988496ab485cf50b836e81b56c5160369e
daniel.domurad <daniel.domurad@makingwaves.com>
2020-09-06 09:55:01 +0200

    cleanup

--- download_mnist_database.sh
+++
@@ -1,5 +0,0 @@
-mkdir ./DIGITS
-wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -O ./DIGITS/train-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -O ./DIGITS/train-labels-idx1-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -O ./DIGITS/t10k-images-idx3-ubyte.gz 
-wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -O ./DIGITS/t10k-labels-idx1-ubyte.gz 

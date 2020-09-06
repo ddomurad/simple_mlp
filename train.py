724fb3cec93a663e532e3db37a25f7a23f1bb3b6
daniel.domurad <daniel.domurad@makingwaves.com>
2020-09-06 09:13:10 +0200

    refactor

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import sys, argparse
+import sys, argparse, random
 import mnist_helper as mnist
 from tqdm import tqdm
 from mlp import MlpBuilder
@@ -7,19 +7,21 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", required=True, help="new - create new mlp network model, load - load existing network model. [eg. '-n new']")
 ap.add_argument("-n", "--network", required=True, help="network file name eg. ['-f ./test_mlp']")
 ap.add_argument("-d", "--mnist_db", required=True, help="MNIST databse dir [eg. '-m ./DIGITS/']")
-ap.add_argument("-f", "--func", required=False, help="activation function type (tanh, ...). [eg. '-a tanh']")
-ap.add_argument("-l", "--layer", required=False, help="add hidden layers of giver size [eg. '-l 10,20'] - two hidden layers of size 10 and 20")
-ap.add_argument("-e", "--epoche", required=True, help="training epoche count [eg. '-e 10']")
-ap.add_argument("-a", "--alpha", required=True, help="training alpha value [eg. '-a 0.1']")
+ap.add_argument("-f", "--func", required=False, help="activation function type (tanh, relu). [eg. '-a tanh']", default="tanh")
+ap.add_argument("-l", "--layer", required=False, help="add hidden layers of giver size [eg. '-l 10,20'] - two hidden layers of size 10 and 20", default="")
+ap.add_argument("-e", "--epoche", required=False, help="training epoche count [eg. '-e 10']", default="10")
+ap.add_argument("-a", "--alpha", required=False, help="training alpha value [eg. '-a 0.1']", default="0.1")
+ap.add_argument("--alpha_decay", required=False, help="training alpha decay value [eg. '--alpha_decay 0.8']", default="0.8")
 args = vars(ap.parse_args())
 
 MLP_LOAD = args['model']
 MNIST_DIR = args['mnist_db']
 MLP_FILE_NAME = args['network']
-ACT_TYPE = args['func'] if args['func'] is not None else 'tanh'
-HIDDEN_LAYERS = [int(l.tring()) for l in args['layer'].split(',')] if args['layer'] is not None else None
+ACT_TYPE = args['func']
+HIDDEN_LAYERS = [int(l.strip()) for l in args['layer'].split(',') if l]
 EPOCH_COUNT = int(args['epoche'])
 MAX_ALPHA = float(args['alpha'])
+ALPHA_DEACY = float(args['alpha_decay'])
 
 print("PARAMS:")
 print("MLP_LOAD", MLP_LOAD)
@@ -29,6 +31,7 @@ print("ACT_TYPE", ACT_TYPE)
 print("HIDDEN_LAYERS", HIDDEN_LAYERS)
 print("EPOCH_COUNT", EPOCH_COUNT)
 print("MAX_ALPHA", MAX_ALPHA)
+print("ALPHA_DEACY", ALPHA_DEACY)
 
 # load training and validation samples
 t_features, t_labels = mnist.load(MNIST_DIR, kind="train")
@@ -37,10 +40,7 @@ v_features, v_labels = mnist.load(MNIST_DIR, kind="t10k")
 # create new mlp
 mlp = None
 if MLP_LOAD == "new":
-    if HIDDEN_LAYERS is None:
-        mlp = MlpBuilder.new_mlp((784, 10), ACT_TYPE)
-    else:
-        mlp = MlpBuilder.new_mlp((784, *HIDDEN_LAYERS, 10), ACT_TYPE)
+    mlp = MlpBuilder.new_mlp((784, *HIDDEN_LAYERS, 10), ACT_TYPE)
 else:
     mlp = MlpBuilder.load_mlp(MLP_FILE_NAME)
 
@@ -68,14 +68,17 @@ for t in range(EPOCH_COUNT):
     print("---------- EPOCH: {} ----------------------------------- ".format(t))
     print("Training (a={})".format(alpha))
     training_avg_error = 0
+    
+    trainign_indices = list(range(len(t_labels))) # 0,1,2,3, ... ,[len(t_labels) - number of trainging samples]
+    random.shuffle(trainign_indices)
 
-    for i in tqdm(range(len(t_labels))):
+    for i in tqdm(trainign_indices):
         x = mnist.normalize(t_features[i])
         y = mnist.get_expected_output(t_labels[i])
         training_avg_error += mlp.train(x, y, alpha)
 
     training_avg_error /= len(t_labels)
-    alpha = alpha*0.80
+    alpha = alpha*ALPHA_DEACY
 
     print("Training avg error = {}".format(training_avg_error))
     print("Validation")

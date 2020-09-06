96245581a8c4e0a5adb2fb4c0499531e7301494d
daniel.domurad <daniel.domurad@makingwaves.com>
2020-09-06 09:54:52 +0200

    download mnist dataset updated

---
+++ DATA/download_mnist_database.sh
@@ -0,0 +1,5 @@
+mkdir ./MNIST
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -O ./MNIST/train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -O ./MNIST/train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -O ./MNIST/t10k-images-idx3-ubyte.gz 
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -O ./MNIST/t10k-labels-idx1-ubyte.gz 
